52c5992119b5875cbce7fac3ee531a0c6732b9f2
Subodh Pandey <subodh.pandey@successive.tech>
2022-02-15 14:51:21 +0530

    Added feature,Data,Query,Step file for Seller Shipping

---
+++ subodh/Data/updateShippingData.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sellerShippingData: {
+      enableExcludeShippingAmount:true,
+      disableExcludeShippingAmount:false,
+      unauthorized:"You are not authorized to perform this action."
+    },
+  };
+  
---
+++ subodh/step/updateSellerShippingSettingStep.js
@@ -0,0 +1,33 @@
+
+const { assert } = require("chai");
+const I = require("../../customSteps");
+
+// query
+const { sellerShipping } = require("../../gqlQuery/sellerShipping/updateSellerShippingSetting");
+
+// data
+const { loginData } = require("../../data/login/loginData");
+const { sellerShippingData } = require("../../data/sellerShipping/sellerShippingData");
+
+When('User is hitting seller shipping setting for isExcludeShippingAmount value as true', async() => {
+    const input = sellerShipping.input({isExcludeShippingAmount:sellerShippingData.enableExcludeShippingAmount});
+    response = await I.sendMutation(sellerShipping.query,input,{},{ token: loginData.token });
+});
+
+Then('User should get seller shipping setting status as ok in response', () => {
+    assert.equal(response.data.data.updateSellerShippingSetting.status, loginData.statusOk);
+});
+
+When('User is hitting seller shipping setting for isExcludeShippingAmount value as false', async() => {
+    const input = sellerShipping.input({isExcludeShippingAmount:sellerShippingData.disableExcludeShippingAmount});
+    response = await I.sendMutation(sellerShipping.query,input,{},{ token: loginData.token });
+});
+
+When('User is hitting seller shipping setting for isExcludeShippingAmount value as null', async() => {
+    const input = sellerShipping.input({isExcludeShippingAmount:sellerShippingData.disableExcludeShippingAmount});
+    response = await I.sendMutation(sellerShipping.query,input,{},{ token: loginData.token });
+});
+
+Then('User should get seller shipping setting error of You are not authorized to perform this action in response', () => {
+    assert.equal(response.data.data.updateSellerShippingSetting.error, sellerShippingData.unauthorized);
+});

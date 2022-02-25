7497e11fe4b59f79494357c9e594f6f2a6146922
Subodh Pandey <subodh.pandey@successive.tech>
2022-02-25 11:50:05 +0530

    test/gql/getProductFormSetting

--- getProductFormSetting/getProductFormSettingData.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  productFormData: {
-    invalidToken : "Invalid token provided.",
-    invalidString: "Invalid token.",
-    seller       : "seller",
-    unAuthorized : "You are not authorized to perform this action."
-  },
-};

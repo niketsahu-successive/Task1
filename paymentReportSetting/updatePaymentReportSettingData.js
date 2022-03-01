a41cfc337f8ab53df85607882d9840eb75127d70
Subodh Pandey <subodh.pandey@successive.tech>
2022-03-01 17:25:19 +0530

    test/gql/updatePaymentReportSetting

--- paymentReportSetting/updatePaymentReportSettingData.js
+++ paymentReportSetting/updatePaymentReportSettingData.js
@@ -4,9 +4,9 @@ const newLabel = faker.name.title();
 module.exports = {
   paymentReportSettingData: {
     newLabel,
-    sameLabelName: "Duplicate data found for label.",
     invalidToken : "Invalid token provided.",
-    unauthorized : "Invalid value provided in user.",
-    invalidString: "Invalid token"
+    invalidString: "Invalid token",
+    sameLabelName: "Duplicate data found for label.",
+    unauthorized : "You are not authorized to perform this action."
   }
 }

d0afc43c035027ba27f9b29cd460f7713e925f2e
qa-Akshat <akshat.rastogi@successive.tech>
2022-03-02 10:35:20 +0530

    Test/gql/get order export setting: Get order export setting endpoint GQL Automation.

--- orderExportSetting/getOrderExportSettingData.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    orderExportSettingData: {
-        unauthorizedError: "You are not authorized to perform this action.",
-        invalidUser      : "You are not authorized to perform this action.",
-        invalidToken     : "Invalidtokenprovidedxcddc_xsuu",
-        invalidTokenError: "Invalid token provided.",
-        seller           : "seller",
-    }
-}
